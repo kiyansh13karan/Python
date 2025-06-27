e261400185f30a2ee8b2ece62600dfcedbd84584
Karan Nayal <somnathtigers2006@gmail.com>
2025-06-27 11:08:16 +0530

    code added in Question_01.py

---
+++ Strings/Practise Set/Question_01.py
@@ -0,0 +1,9 @@
+# write a python program to display a user entered name followed by good morning using input() function 
+
+
+# Get user input for name
+name = input("Enter your name: ")
+
+# Display the name followed by "Good Morning" using escape sequence characters
+print("Good Morning " + name + "!")
+

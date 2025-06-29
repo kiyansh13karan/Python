16e2d8bf8e2341163dc3f1283720a35fad2bf2e2
Karan Nayal <somnathtigers2006@gmail.com>
2025-06-29 12:25:31 +0530

    code added to Question_02.py

--- Dictionary and Sets/Practice Set/Question_02.py
+++ Dictionary and Sets/Practice Set/Question_02.py
@@ -1,18 +1,18 @@
 # write a python program to to input eight numbers from the user and display all the unique numbers (once)
 
-def get_unique_numbers():
+def get_unique_numbers() :
     print("Enter eight numbers (press Enter after each number):")
     unique_numbers = set()  # Using a set to store unique numbers
 
-    for _ in range(8):
+    for _ in range(8) :
         try:
             number = int(input())
             unique_numbers.add(number)  # Add the number to the set
         except ValueError:
             print("Please enter a valid integer.")
 
-    print("Unique numbers entered:", unique_numbers)
+    print("Unique numbers entered :", unique_numbers)
 
-if __name__ == "__main__":
+if __name__ == "__main__" :
     get_unique_numbers()
     

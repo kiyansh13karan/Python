4f5dd04db6fca8381aded7ea0908a62a373d63e9
Karan Nayal <somnathtigers2006@gmail.com>
2025-07-01 10:11:17 +0530

    code added to if_else_ladder.py

---
+++ Conditional Expression/if_else_ladder.py
@@ -0,0 +1,31 @@
+# if else conditions of even odd testing 
+num = int(input("Enter a number : ")) 
+
+if(num % 2 == 0) :
+    print("Number is even")
+else :
+    print("Number is Odd") 
+
+
+
+
+# Check if a number is positive or negative
+num = int(input("Enter a number: "))
+
+if num >= 0:
+    print("The number is positive.")
+else:
+    print("The number is negative.")
+
+
+
+
+
+# Find the greatest of two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("First number is greater.")
+else:
+    print("Second number is greater or equal.")

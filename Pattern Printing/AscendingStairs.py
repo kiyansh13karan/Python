f83ab1772c62358c2f9206bae8a2f3f1ee089591
Karan Nayal <somnathtigers2006@gmail.com>
2025-07-01 23:47:27 +0530

    code editted of AscendingStairs.py

--- Pattern Printing/AscendingStairs.py
+++ Pattern Printing/AscendingStairs.py
@@ -6,8 +6,8 @@
 # * * * * * *
 # * * * * * * * 
 
-num = int(input("Enter the number of Rows : ")) 
-for i in range(1 , num+1) :
-    for j in range(1 , i+1) :
-        print("*" , end=" ")
-    print()
+num = int(input("Enter the number of lines : "))
+
+for i in range(1 , num+1) : 
+    print("*"*  (i) , end=" ")
+    print("")

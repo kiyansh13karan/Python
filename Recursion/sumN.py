812a71ca40c0c788121659e532d165bd87b21905
Karan Nayal <somnathtigers2006@gmail.com>
2025-07-02 01:43:51 +0530

    Code editted

--- Recursion/sumN.py
+++ Recursion/sumN.py
@@ -1,12 +1,12 @@
 # write a progrsm to calculate sum of first n number , using recursion :-
 
 # function definition :_
-def cal_sum(n) :
+def sum(n) :
     if(n==0) :
         return 0 
-    return cal_sum(n-1) + n 
+    return sum(n-1) + n 
 
 # main function :-
 num = int(input("Enter n number : ")) 
-res = cal_sum(num)
+res = sum(num)
 print("Sum of first inputed natural number is : " , res)  

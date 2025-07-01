aae7ba09078b4fbe4f187f4e55c654b2cd5e66d8
Karan Nayal <somnathtigers2006@gmail.com>
2025-07-01 10:40:40 +0530

    code added to relational_operator.py

---
+++ Conditional Expression/relational_operator.py
@@ -0,0 +1,24 @@
+# Relational operators 
+'''
+Relational operators are used to evalute conditions inside the if statements.
+Some examples of relational operators are :-
+| Operator | Description           | Example  |
+| -------- | --------------------- | -------- |
+| `==`     | Equal to              | `a == b` |
+| `!=`     | Not equal to          | `a != b` |
+| `>`      | Greater than          | `a > b`  |
+| `<`      | Less than             | `a < b`  |
+| `>=`     | Greater than or equal | `a >= b` |
+| `<=`     | Less than or equal    | `a <= b` |
+'''
+
+#examples of relational operators
+
+a = 10
+b = 20
+print("a == b:", a == b)    # False
+print("a != b:", a != b)    # True
+print("a > b:", a > b)      # False
+print("a < b:", a < b)      # True
+print("a >= b:", a >= b)    # False
+print("a <= b:", a <= b)    # True

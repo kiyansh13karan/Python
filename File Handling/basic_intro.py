d2d24c993be92f8a841e4c5f9813bc181793c204
Karan Nayal <somnathtigers2006@gmail.com>
2025-07-03 19:42:58 +0530

    content added in basic_intro.py

---
+++ File Handling/basic_intro.py
@@ -0,0 +1,13 @@
+# File I/O in Python allows you to read from and write to files on your computer. Here's a complete overview:
+
+'''
+File operations in Python include:
+
+'r'	    => Read (default). File must exist.
+'w'	    => Write. Creates new or overwrites existing file.
+'a'	    => Append. Adds content to the end of file.
+'r+'	=> Read and Write. File must exist.
+'w+'	=> Write and Read. Overwrites file.
+'a+'	=> Append and Read. Creates file if not exists.
+'b'	    => Binary mode (e.g., 'rb', 'wb').
+'''

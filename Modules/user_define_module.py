14f15562c8532e3ccd4225c174f6715ca4eb8489
Karan Nayal <somnathtigers2006@gmail.com>
2025-07-13 10:34:05 +0530

    code added

---
+++ Modules/user_define_module.py
@@ -0,0 +1,20 @@
+# User defined module in python :-
+
+'''
+A user-defined module is a Python file (with a .py extension) that you create yourself. It contains functions, classes, or variables that you can use in other Python programs by importing the file.
+
+This helps make your code:
+Reusable ✅
+Organized 📦
+Clean and modular 🧩
+'''
+
+
+#  Example: Creating a User-Defined Module : -
+# mymodule.py
+
+def greet(name):
+    return f"Hello, {name}!"
+
+def add(x, y):
+    return x + y

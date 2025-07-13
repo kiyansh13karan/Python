c896426b70338ad2fb5a1f28e045916a9747a5a0
Karan Nayal <somnathtigers2006@gmail.com>
2025-07-13 10:28:10 +0530

    code added in built_in_module.py

---
+++ Modules/built_in_module.py
@@ -0,0 +1,12 @@
+# Built in module in python :- 
+
+'''
+Built-in modules in Python are modules that come pre-installed with Python. You don’t need to install them separately — just import and use them.
+
+They are part of the Python Standard Library, which is a large collection of useful tools for file handling, math, time, system interaction, and more.
+'''
+
+
+import math
+print(f"Square root of 25 is : {math.sqrt(25)}")  # Output: 5.0
+print(f"Square root of 81 is : {math.sqrt(81)}")  # Output: 9.0
